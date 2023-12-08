4c47296033974e9357d56f6b87798fe1eb975a5c
fgael <gaelfoini@gmail.com>
2023-12-08 10:13:59 +0100

    make profile route private

--- frontend/src/router/index.jsx
+++ frontend/src/router/index.jsx
@@ -1,10 +1,25 @@
-// Modifiez votre fichier router/index.jsx
-
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import Login from "../pages/Login/Login";
 import Profile from "../pages/Profile/Profile";
 import Error404 from "../pages/Error404";
+import { useSelector } from "react-redux";
+import { selectIsAuthenticated } from "../state/auth/authSlice";
+import PropTypes from "prop-types";
+
+const PrivateRoute = ({ element }) => {
+  const isAuthenticated = useSelector(selectIsAuthenticated);
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" />;
+  }
+
+  return element;
+};
+
+PrivateRoute.propTypes = {
+  element: PropTypes.node.isRequired,
+};
 
 const AppRouter = () => {
   return (
@@ -12,7 +27,10 @@ const AppRouter = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route
+          path="/profile"
+          element={<PrivateRoute element={<Profile />} />}
+        />
         <Route path="*" element={<Error404 />} />
       </Routes>
     </BrowserRouter>
